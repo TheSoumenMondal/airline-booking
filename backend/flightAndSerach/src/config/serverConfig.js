dd433e782ad3032aecb5cb1ff33d178843da627c
TheSoumenMondal <thesoumenmondal50@gmail.com>
2025-08-14 21:45:43 +0530

    Set up Sequalized and postgresql

--- backend/flightAndSerach/src/config/serverConfig.js
+++ backend/flightAndSerach/src/config/serverConfig.js
@@ -1,5 +1,10 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const serverConfig = {
     PORT : process.env.PORT || 3000,
+    DB_URL: process.env.DB_URL
 }
 
 export default serverConfig;
--- backend/flightAndSerach/src/index.js
+++ backend/flightAndSerach/src/index.js
@@ -4,6 +4,6 @@ import serverConfig from "./config/serverConfig.js";
 const PORT = serverConfig.PORT;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`✈️  Flight and Search Server is running on port ${PORT}`);
 });
 
