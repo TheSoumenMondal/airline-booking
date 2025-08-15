1224d669150fe849e46395b7bce2afb7925659c8
TheSoumenMondal <thesoumenmondal50@gmail.com>
2025-08-15 10:52:01 +0530

    Added service layer

--- backend/flightAndSerach/src/repository/cityRepository.js
+++ backend/flightAndSerach/src/repository/cityRepository.js
@@ -1,25 +1,48 @@
-const {City} = require("../../models/index")
+const { City } = require("../../models/index");
 
-class CityRepository{
-    async createCity({name}){
-        try {
-            const city = await City.create({ name });
-            return city;
-        } catch (error) {
-            throw error;
-        }
+class CityRepository {
+  async createCity({ name }) {
+    try {
+      const city = await City.create({ name });
+      return city;
+    } catch (error) {
+      throw {error};
     }
-    async deletCity({id}){
-        try {
-            await City.destroy({
-                where:{
-                    id : id
-                }
-            })
-        } catch (error) {
-            throw error;
-        }
+  }
+
+  async deleteCity({ id }) {
+    try {
+      await City.destroy({
+        where: {
+          id: id,
+        },
+      });
+    } catch (error) {
+      throw {error};
     }
+  }
+
+  async getCity({ cityId }) {
+    try {
+      const city = await City.findByPk(cityId);
+      return city;
+    } catch (error) {
+      throw {error};
+    }
+  }
+
+  async updateCity(cityId, data) {
+    try {
+      const city = await City.update(data, {
+        where: {
+          id: cityId,
+        },
+      });
+      return city;
+    } catch (error) {
+      throw {error};
+    }
+  }
 }
 
 module.exports = CityRepository;
